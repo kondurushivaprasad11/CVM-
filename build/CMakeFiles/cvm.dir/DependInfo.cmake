d026b8ebca7db51e2e9ca750395093396c149048
kondurushivaprasad11 <kondurushivaprasad11@gmail.com>
2026-05-17 00:36:21 +0530

    Add files via upload

---
+++ build/CMakeFiles/cvm.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/shiva prasad/OneDrive/Desktop/CVM++/src/ast/ASTPrinter.cpp" "CMakeFiles/cvm.dir/src/ast/ASTPrinter.cpp.obj" "gcc" "CMakeFiles/cvm.dir/src/ast/ASTPrinter.cpp.obj.d"
+  "C:/Users/shiva prasad/OneDrive/Desktop/CVM++/src/compiler/Compiler.cpp" "CMakeFiles/cvm.dir/src/compiler/Compiler.cpp.obj" "gcc" "CMakeFiles/cvm.dir/src/compiler/Compiler.cpp.obj.d"
+  "C:/Users/shiva prasad/OneDrive/Desktop/CVM++/src/lexer/Lexer.cpp" "CMakeFiles/cvm.dir/src/lexer/Lexer.cpp.obj" "gcc" "CMakeFiles/cvm.dir/src/lexer/Lexer.cpp.obj.d"
+  "C:/Users/shiva prasad/OneDrive/Desktop/CVM++/src/main.cpp" "CMakeFiles/cvm.dir/src/main.cpp.obj" "gcc" "CMakeFiles/cvm.dir/src/main.cpp.obj.d"
+  "C:/Users/shiva prasad/OneDrive/Desktop/CVM++/src/parser/Parser.cpp" "CMakeFiles/cvm.dir/src/parser/Parser.cpp.obj" "gcc" "CMakeFiles/cvm.dir/src/parser/Parser.cpp.obj.d"
+  "C:/Users/shiva prasad/OneDrive/Desktop/CVM++/src/vm/Debug.cpp" "CMakeFiles/cvm.dir/src/vm/Debug.cpp.obj" "gcc" "CMakeFiles/cvm.dir/src/vm/Debug.cpp.obj.d"
+  "C:/Users/shiva prasad/OneDrive/Desktop/CVM++/src/vm/VM.cpp" "CMakeFiles/cvm.dir/src/vm/VM.cpp.obj" "gcc" "CMakeFiles/cvm.dir/src/vm/VM.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
